0e8817ac1020ae19611baac604242895ae1b8b1e
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-08-03 15:17:14 -0400

    add title

--- src/info.js
+++ src/info.js
@@ -2,6 +2,11 @@ import triumph from "./assets/triumph.png";
 import storybook from "./assets/storybook.png";
 import snapchat from "./assets/snapchat.png";
 import slack from "./assets/slack.png";
+import linkedin from "./assets/linkedin.png";
+import splash from "./assets/splash.png";
+import delectable from "./assets/delectable.png";
+import aboveall from "./assets/aboveall.png";
+import bakery from "./assets/bakery.png";
 
 export const DATA = [
   {
@@ -24,10 +29,46 @@ export const DATA = [
     img: snapchat,
     url: "https://snapchat-clone-bb3e6.web.app/",
   },
+
   {
     name: "Slack Clone",
     info: "Slack Clone is a clone of Slack made with React, Redux, Material UI, and Firebase!",
     img: slack,
     url: "https://slack-clone-23df2.firebaseapp.com/",
+  },
+
+  {
+    name: "LinkedIn Clone",
+    info: "LinkedIn is a clone of LinkedIn made with React, Redux, Material UI, and Firebase!",
+    img: linkedin,
+    url: "https://linked-in-clone-d75b2.web.app/",
+  },
+
+  {
+    name: "Splash",
+    info: "Splash is a website made with HTML, SCSS, React, and Context. You can search for any cocktail and you will get the ingredients, glass-type, and instructions!",
+    img: splash,
+    url: "https://splashdb.netlify.app/",
+  },
+
+  {
+    name: "Delectable",
+    info: "Delectable is a website made with HTML, SCSS, React, and Context. You can search for any meal and it will give you the recipe, and instructions on how to make it!",
+    img: delectable,
+    url: "https://delectable-app.netlify.app/",
+  },
+
+  {
+    name: "Kristen's Bakery",
+    info: "Kristen's Bakery is a website made with HTML, SCSS, React, Redux, Nodejs, Materialize, Firebase and Stripe. You can purchase baked goods using stripe!",
+    img: bakery,
+    url: "https://still-fortress-50463.herokuapp.com/",
+  },
+
+  {
+    name: "Above All Pest Control",
+    info: "Above All Pest Control is a pest control company located in Langhorne, PA. This website was made with HTML, CSS, Bootstrap, Nodejs, and Mailgun!",
+    img: aboveall,
+    url: "https://delectable-app.netlify.app/",
   },
 ];
